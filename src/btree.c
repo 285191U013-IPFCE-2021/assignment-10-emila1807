c4388b74848fc4491a21321b4cd4f304562e270a
Emil <91883791+emila1807@users.noreply.github.com>
2021-11-18 19:44:29 +0100

    Add files via upload
    let's go

--- src/btree.c
+++ src/btree.c
@@ -9,6 +9,7 @@
 struct tree_node * Insert (int x, struct tree_node *t)
 {
   // Insert item x into the tree t
+  *t = *t + x;
 
   return NULL;
 }
@@ -17,7 +18,10 @@ struct tree_node * Remove (int x, struct tree_node *t)
 {
  // Remove one item from the tree t
 
+  if (x == NULL)
   return NULL;
+  else (x = x - x);
+
 }
 
 
@@ -25,6 +29,11 @@ int Contains (int x, struct tree_node *t)
 {
 
   // Return true if the tree t contains item x. Return false otherwise.
+if (x != NULL) {
+  return true;
+}
+else
+
 
   return NULL;
 
--- src/linked_list.c
+++ src/linked_list.c
@@ -38,18 +38,51 @@ void print_list (node * p)
 {
     // Add your code for exercise 1
     // There is NO testcode for this
+
+    if (SENTINEL_node == NULL) { // If the list is empty then nothing will be printed
+    printf("Null");}
+    else {
+      printf("%d, value"); // Print the value
+      print_list(value->next); // Recurssion
+
+      return sum;
+    }
 }
 
 int sum_squares (node * p)
 {
     // Add your code for excercise 2
     // You can find the tests in tests.cpp
+
+int sum;
+
+node* list =
+  make_node(1,
+    make_node(2,
+      make_node(3,
+        make_node(4,
+          make_node(5, &SENTINEL)
+
+      )
+    )
+  )
+);
+
+  sum = sum_squares(list); /* sum should equal 55 */
+
     return -1;
 }
 
 node *map (node * p, int (*f) (int))
 {
     // Add your code for excercise 3
+
+int (*sf)(int);
+sf = square;
+int x;
+x = sf(2);
+
+
     return NULL;
 }
 
